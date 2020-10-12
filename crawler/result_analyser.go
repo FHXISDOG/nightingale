0c7658ae491a14cda056d1d8dc3711f2264d561a
finger <fingergohappy@163.com>
2020-10-12 16:41:45 +0800

    add xml query result functions

--- crawler/result_analyser.go
+++ crawler/result_analyser.go
@@ -1,6 +1,11 @@
 package crawler
 
-import "io"
+import (
+	"fmt"
+	"io"
+
+	"github.com/antchfx/xmlquery"
+)
 
 type ParseResult struct {
 	Title, Author, Description, Content, Link, Source string
@@ -8,12 +13,31 @@ type ParseResult struct {
 }
 
 type XmlRule struct {
-	ParentNode, TitleNode, DescriptionNode, ContentNode, LineNode, DateNode string
+	ParentNode, TitleNode, DescriptionNode, ContentNode, LinkNode, DateNode string
 }
 
 // parse xml
-func xml_parser(source io.Reader, rule XmlRule) ParseResult {
-	var result ParseResult
+func xml_parser(source io.Reader, rule XmlRule) []*ParseResult {
+	result := make([]*ParseResult, 100)
+	doc, err := xmlquery.Parse(source)
+	if err != nil {
+		fmt.Println("parse xml error", err)
+	}
+	parentNode := xmlquery.find(doc, rule.ParentNode)
+	for _, val := range parentNode {
+		title := val.SelectElement(rule.TitleNode).InnerText()
+		description := val.SelectElement(rule.DescriptionNode).InnerText()
+		content := val.SelectElement(rule.ContentNode).InnerText()
+		link := val.SelectElement(rule.LinkNode).InnerText()
+		date := val.SelectElement(rule.DateNode).InnerText()
 
+		result = result.append(&ParseResult{
+			Title:       title,
+			Content:     content,
+			Description: description,
+			Date:        date,
+			Link:        link,
+		})
+	}
 	return result
 }
