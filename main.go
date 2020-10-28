466e81349d3ae40ce1fb040917f409514755e3ce
finger <fingergohappy@163.com>
2020-10-28 17:01:46 +0800

    recover gin

--- main.go
+++ main.go
@@ -3,14 +3,12 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
+	r := gin.Default()
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"messgae": "pong",
 		})
-	})
-	//test msg
-	r.GET("/test", func(c *gin.Context) {
-
 	})
 
 	r.Run()
